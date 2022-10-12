27c577d8cfd57b849dd3a7f4cb3a78ae19dd183c
Cao Minh Tri <caominhtri112201@gmail.com>
2022-10-12 15:45:41 +0700

    [BT-9] Implement Footer

--- client/src/page/Home/Home.js
+++ client/src/page/Home/Home.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Header from "../../common/Header/Header";
+import Footer from "../../common/Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Header />
+            Content
+            <Footer />
         </div>
     );
 };
