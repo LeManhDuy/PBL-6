f17fa7fe52cb3a010c9d9657e321230266fa62f3
Cao Minh Tri <caominhtri112201@gmail.com>
2022-10-27 17:49:42 +0700

    [PBL-46] Fix Route

---
+++ client/src/page/Parents/StudentParents/StudentParents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentParents = () => {
+    return <div>StudentParents</div>;
+};
+
+export default StudentParents;
---
+++ client/src/page/Teacher/ScheduleTeacher/ScheduleTeacher.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScheduleTeacher = () => {
+    return <div>ScheduleTeacher</div>;
+};
+
+export default ScheduleTeacher;
