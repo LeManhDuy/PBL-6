bc2f3a7b85a0eb42484362f2f400342f1bd1c5cb
Cao Minh Tri <caominhtri112201@gmail.com>
2022-11-02 16:32:32 +0700

    [PBL-62] Create Notification Service

---
+++ client/src/config/service/NotificationService.js
@@ -0,0 +1,61 @@
+import HandleApi from "../api/HandleAPI";
+
+const getNotifications = async () => {
+    return await HandleApi.APIGetWithToken(`notification`);
+};
+
+const getNotificationsParents = async (id) => {
+    return await HandleApi.APIGetWithToken(
+        `notification/private/parents/${id}`
+    );
+};
+
+const getNotificationsTeacher = async (id) => {
+    return await HandleApi.APIGetWithToken(`notification/private/${id}`);
+};
+
+const getNotificationsById = async (id) => {
+    return await HandleApi.APIGetWithToken(`notification/${id}`);
+};
+
+const createPublicNotification = async (params) => {
+    return await HandleApi.APIPostWithToken(`notification`, params);
+};
+
+const createPrivateNotification = async (params) => {
+    return await HandleApi.APIPostWithToken(`notification/private`, params);
+};
+
+const updatePublicNotification = async (params, id) => {
+    return await HandleApi.APIPutWithToken(`notification/${id}`, params);
+};
+
+const updatePrivateNotification = async (params, id) => {
+    return await HandleApi.APIPutWithToken(
+        `notification/private/${id}`,
+        params
+    );
+};
+
+const deletePublicNotification = async (id) => {
+    return await HandleApi.APIDelete(`notification/${id}`);
+};
+
+const deletePrivateNotification = async (id) => {
+    return await HandleApi.APIDelete(`notification/private/${id}`);
+};
+
+const NotificationService = {
+    getNotifications,
+    getNotificationsParents,
+    getNotificationsTeacher,
+    getNotificationsById,
+    createPublicNotification,
+    createPrivateNotification,
+    updatePublicNotification,
+    updatePrivateNotification,
+    deletePublicNotification,
+    deletePrivateNotification,
+};
+
+export default NotificationService;
