c55c7cd6e2aa9234459567bd86530ef6e712ef81
Nguyễn Khoa Hoàng <blcm2486@gmail.com>
2022-11-17 12:32:41 +0700

    77 unit test

--- server/test/class.js
+++ server/test/class.js
@@ -9,6 +9,7 @@ let server = require("../server");
 let should = chai.should();
 
 const API = "http://localhost:8000";
+const APIClass = '/api/class/';
 
 chai.use(chaiHttp);
 //Our parent block
@@ -23,10 +24,25 @@ describe("Classes", () => {
     describe("/GET classes", () => {
         it("it should GET all the classes", (done) => {
             chai.request(API)
-                .get("/api/class")
+                .get(APIClass)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.allClass.should.be.a("array");
+                    expect(res.body.success).to.be.true;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET/:class classes', () => {
+        it('it should GET a class by the given id', (done) => {
+            // TODO add a model to db then get that *id* to take this test
+            let id = '635e2957b41c372de36e3330';
+            chai.request(API)
+                .get(APIClass + id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.getClassInfor.should.be.a("array");
                     expect(res.body.success).to.be.true;
                     done();
                 });
